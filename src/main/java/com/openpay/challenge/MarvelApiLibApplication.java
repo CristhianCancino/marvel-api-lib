29dc84ba92d7f72d971ae474281ea19c68313b73
cristhiancll <cristhian.cancino1984@gmail.com>
2024-06-26 22:31:16 -0300

    primer commit

---
+++ src/main/java/com/openpay/challenge/MarvelApiLibApplication.java
@@ -0,0 +1,13 @@
+package com.openpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openpay/challenge/MarvelApiLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
